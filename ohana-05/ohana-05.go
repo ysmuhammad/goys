e19a4beb8066c34f6fc4a47ecf10195f6cc5d887
ysmuhammad <yusuf.ekaputera@gmail.com>
2020-06-17 16:49:34 +0700

    ohana-06

--- ohana-05/ohana-05.go
+++ ohana-05/ohana-05.go
@@ -4,29 +4,29 @@ import (
 	"fmt"
 )
 
-type heaven struct{
+type heaven struct {
 	country string
-	city string
+	city    string
 }
 
-type football struct{
+type football struct {
 	clubName string
-	country string
-	region string
-	players []string
+	country  string
+	region   string
+	players  []string
 }
 
 func main() {
 	heav1 := heaven{
 		country: "Hawaii",
-		city: "Oahu",
+		city:    "Oahu",
 	}
 
 	heav2 := heaven{
 		country: "Indonesia",
-		city: "Mentawai",
+		city:    "Mentawai",
 	}
-	
+
 	fmt.Println(heav1, heav2)
 	fmt.Printf("Best city to surf in Indonesia? \n- %v\n", heav2.city)
 	fmt.Printf("Best city to surf in Hawaii? \n- %v\n", heav1.city)
@@ -39,15 +39,15 @@ func main() {
 }
 
 func nestedStruct() {
-	type boss struct{
+	type boss struct {
 		firstName string
-		lastName string
+		lastName  string
 	}
-	type kids struct{
+	type kids struct {
 		firstName string
-		lastName string
+		lastName  string
 	}
-	type fam struct{
+	type fam struct {
 		boss
 		kids
 		status string
@@ -55,17 +55,17 @@ func nestedStruct() {
 
 	boss1 := boss{
 		firstName: "John",
-		lastName: "Doe",
+		lastName:  "Doe",
 	}
 
 	kids1 := kids{
 		firstName: "Greenwood",
-		lastName: "Mason",
+		lastName:  "Mason",
 	}
 
 	all := fam{
-		boss: boss1,
-		kids: kids1,
+		boss:   boss1,
+		kids:   kids1,
 		status: "Happy Family!",
 	}
 
@@ -74,22 +74,22 @@ func nestedStruct() {
 	fmt.Printf("Family status: %v\n", all.status)
 }
 
-func structSlice(){
-	type jkt struct{
-		area string
-		station string
+func structSlice() {
+	type jkt struct {
+		area       string
+		station    string
 		population []int
 	}
 
 	v1 := jkt{
-		area: "Lebak Bulus",
+		area:    "Lebak Bulus",
 		station: "Fatmawati MRT Station",
 		population: []int{
 			15094,
 			99999,
 		},
 	}
-	fmt.Printf("=========\nArea: %v\nStation Name: %v\nPopulation: %d\n", v1.area, v1.station,v1.population[1])
+	fmt.Printf("=========\nArea: %v\nStation Name: %v\nPopulation: %d\n", v1.area, v1.station, v1.population[1])
 	fmt.Println("========")
 }
 
---
+++ ohana-06/ohana-06.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+)
+
+type players struct {
+	firstName string
+	lastName  string
+	age       int
+	cars
+}
+
+type cars struct {
+	tipe  string
+	color string
+}
+
+func main() {
+	xi := []int{
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	}
+	fmt.Println(xi)
+
+	dv, ys := sl("alana", xi...)
+	fmt.Printf("Momma: %v\nTotal: %v", dv, ys)
+
+	roo := players{
+		firstName: "Rooney",
+		lastName:  "Wayne",
+		age:       30,
+		cars: cars{
+			tipe:  "Mercedes Benz",
+			color: "Red",
+		},
+	}
+
+	merci := cars{
+		tipe:  "Mercedez Benz",
+		color: "White",
+	}
+
+	merci.score()
+	roo.score()
+
+	champ(roo)
+	champ(merci)
+}
+
+func sl(y string, x ...int) (int, string) {
+
+	defer fmt.Println("End of sl function!\n==============")
+	ys := 0
+	for _, v := range x {
+		fmt.Println(v)
+		ys += v
+	}
+	tmpAlana := y
+	anuy := fmt.Sprintf("Binary: %b \n\n", byte(tmpAlana[0]))
+	fmt.Printf("\nType of anuy: %T\nValue of anuy: %v", anuy, anuy)
+
+	return ys, anuy
+}
+
+func (s players) score() {
+	fmt.Printf("Scorer: %v\n", s.firstName)
+	fmt.Printf("Age: %v\n", s.age)
+	fmt.Printf("Car: %v\n", s.tipe)
+	fmt.Printf("Car color: %v\n", s.color)
+}
+
+func (s cars) score() {
+	fmt.Printf("Car: %v\n", s.tipe)
+	fmt.Printf("Car color: %v\n", s.color)
+}
+
+type champions interface {
+	score()
+}
+
+func champ(c champions) {
+	switch c.(type) {
+	case players:
+		fmt.Printf("\nYes you are the champion, %v %v!", c.(players).lastName, c.(players).firstName)
+	case cars:
+		fmt.Printf("\nBest car out there is: %v %v", c.(cars).color, c.(cars).tipe)
+	}
+}
